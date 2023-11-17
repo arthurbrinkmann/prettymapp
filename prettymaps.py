77572cea0e10acb98881232f4d110cb6300a072e
Arthur Brinkmann <arthur.brinkmann@rgi-corp.com>
2023-11-17 09:43:38 -0600

    Adding remaining files

---
+++ prettymaps.py
@@ -0,0 +1,15 @@
+from prettymapp.geo import get_aoi
+from prettymapp.osm import get_osm_geometries
+from prettymapp.plotting import Plot
+from prettymapp.settings import STYLES
+
+aoi = get_aoi(address="1060 W Addison St, Chicago, IL 60613", radius=500, rectangular=False)
+df = get_osm_geometries(aoi=aoi)
+
+fig = Plot(
+    df=df,
+    aoi_bounds=aoi.bounds,
+    draw_settings=STYLES["Auburn"]
+).plot_all()
+
+fig.savefig("map.jpg")
